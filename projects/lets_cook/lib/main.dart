d9d48309fcf4d3cdd68b4540524f845d5c38c813
Amanda Cleto <amanda.clet@gmail.com>
2022-01-07 16:20:19 -0300

    organized project

--- projects/lets_cook/lib/main.dart
+++ projects/lets_cook/lib/main.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:lets_cook/data/dummy_data.dart';
+import 'package:lets_cook/model/meal.dart';
+import 'package:lets_cook/model/settings.dart';
 import 'package:lets_cook/screens/categories_meals_screen.dart';
-import 'package:lets_cook/screens/categories_screens.dart';
+import 'package:lets_cook/screens/meal_detail_screen.dart';
+import 'package:lets_cook/screens/settings_screen.dart';
+import 'package:lets_cook/screens/tabs_screen.dart';
 import 'package:lets_cook/theme/pallete.dart';
 import 'package:lets_cook/utils/app_routes.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  final List<Meal> _favoriteMeals = [];
+
+  Settings settings = Settings();
+
+  void _filterMeals(Settings settings) {
+    setState(() {
+      this.settings = settings;
+
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        final filterGluten = settings.isGlutenFree && !meal.isGlutenFree;
+        final filterLactose = settings.isLactoseFree && !meal.isLactoseFree;
+        final filterVegan = settings.isVegan && !meal.isVegan;
+        final filterVegetarian = settings.isVegetarian && !meal.isVegetarian;
+        return !filterGluten &&
+            !filterLactose &&
+            !filterVegan &&
+            !filterVegetarian;
+      }).toList();
+    });
+  }
+
+  void _toggleFavorite(Meal meal) {
+    setState(() {
+      _favoriteMeals.contains(meal)
+          ? _favoriteMeals.remove(meal)
+          : _favoriteMeals.add(meal);
+    });
+  }
+
+  bool _isFavorite(Meal meal) {
+    return _favoriteMeals.contains(meal);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,16 +59,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(
           primarySwatch: Palette.kToDark,
-        ).copyWith(
-          secondary: Colors.amber,
         ),
-        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        canvasColor: const Color.fromRGBO(237, 225, 223, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
               headline1: const TextStyle(
-                fontSize: 20,
+                fontSize: 22,
                 fontFamily: 'RobotoCondensed',
-                color: Colors.black,
+                color: Colors.black87,
                 fontWeight: FontWeight.normal,
               ),
             ),
@@ -36,9 +78,22 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const CategoriesScreen(),
+      initialRoute: AppRoutes.HOME,
       routes: {
-        AppRoutes.CATEGORIES_MEALS: (contexto) => CategoriesMealsScreen(),
+        AppRoutes.HOME: (contexto) => TabsScreen(
+              favoriteMeals: _favoriteMeals,
+            ),
+        AppRoutes.CATEGORIES_MEALS: (contexto) => CategoriesMealsScreen(
+              meals: _availableMeals,
+            ),
+        AppRoutes.MEAL_DETAIL: (contexto) => MealDetailScreen(
+              onToggleFavorite: _toggleFavorite,
+              isFavorite: _isFavorite,
+            ),
+        AppRoutes.SETTINGS: (contexto) => SettingsScreen(
+              onSettingsChanged: _filterMeals,
+              settings: settings,
+            )
       },
     );
   }
--- projects/lets_cook/lib/screens/categories_screens.dart
+++ projects/lets_cook/lib/screens/categories_screens.dart
@@ -7,26 +7,24 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Let\'s Cook!'),
+    return GridView(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 15,
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(15),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: DUMMY_CATEGORIES.map(
-          (category) {
-            return CategoryItem(
-              category: category,
-            );
-          },
-        ).toList(),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
+      children: DUMMY_CATEGORIES.map(
+        (category) {
+          return CategoryItem(
+            category: category,
+          );
+        },
+      ).toList(),
     );
   }
 }
