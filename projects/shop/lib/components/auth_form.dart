a9f195cfe3e68ef1e8e6612bba7345f4a92105ee
Amanda Cleto <amanda.clet@gmail.com>
2022-01-14 14:45:50 -0300

    layout fix

--- projects/shop/lib/components/auth_form.dart
+++ projects/shop/lib/components/auth_form.dart
@@ -3,10 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:shop/exceptions/auth_exception.dart';
 import 'package:shop/models/auth.dart';
 
-enum AuthMode {
-  Signup,
-  Login,
-}
+enum AuthMode { Signup, Login }
 
 class AuthForm extends StatefulWidget {
   const AuthForm({Key? key}) : super(key: key);
@@ -19,19 +16,19 @@ class _AuthFormState extends State<AuthForm>
     with SingleTickerProviderStateMixin {
   final _passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
+  AuthMode _authMode = AuthMode.Login;
   final Map<String, String> _authData = {
     'email': '',
     'password': '',
   };
 
-  bool _isLoading = false;
-  AuthMode _authMode = AuthMode.Login;
-
   AnimationController? _animationController;
-  Animation<Size>? _heighAnimation;
+  Animation<double>? _opacityAnimation;
+  Animation<Offset>? _slideAnimation;
 
   bool _isLogin() => _authMode == AuthMode.Login;
-  bool _isSignup() => _authMode == AuthMode.Signup;
+  // bool _isSignup() => _authMode == AuthMode.Signup;
 
   @override
   void initState() {
@@ -44,9 +41,9 @@ class _AuthFormState extends State<AuthForm>
       ),
     );
 
-    _heighAnimation = Tween(
-      begin: const Size(double.infinity, 325),
-      end: const Size(double.infinity, 400),
+    _opacityAnimation = Tween(
+      begin: 0.0,
+      end: 1.0,
     ).animate(
       CurvedAnimation(
         parent: _animationController!,
@@ -54,7 +51,17 @@ class _AuthFormState extends State<AuthForm>
       ),
     );
 
-    // _heighAnimation?.addListener(() => setState(() {}));
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, -1.5),
+      end: const Offset(0, 0),
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController!,
+        curve: Curves.linear,
+      ),
+    );
+
+    // _opacityAnimation?.addListener(() => setState(() {}));
   }
 
   @override
@@ -98,8 +105,6 @@ class _AuthFormState extends State<AuthForm>
       return;
     }
 
-    setState(() => _isLoading = true);
-
     _formKey.currentState?.save();
 
     Auth auth = Provider.of(context, listen: false);
@@ -137,7 +142,7 @@ class _AuthFormState extends State<AuthForm>
         duration: const Duration(milliseconds: 300),
         curve: Curves.easeIn,
         height: _isLogin() ? 310 : 400,
-        // height: _heighAnimation?.value.height ?? (_isLogin() ? 310 : 400),
+        // height: _opacityAnimation?.value.height ?? (_isLogin() ? 310 : 400),
         width: deviceSize.width * 0.75,
         padding: const EdgeInsets.all(16),
         child: Form(
@@ -172,21 +177,35 @@ class _AuthFormState extends State<AuthForm>
                         return null;
                       },
               ),
-              if (_isSignup())
-                TextFormField(
-                  decoration:
-                      const InputDecoration(labelText: 'Confirmar Senha'),
-                  keyboardType: TextInputType.emailAddress,
-                  obscureText: true,
-                  validator: (_password) {
-                    final password = _password ?? '';
-                    if (password != _passwordController.text) {
-                      return 'Senhas informadas não conferem.';
-                    }
-
-                    return null;
-                  },
+              AnimatedContainer(
+                constraints: BoxConstraints(
+                  minHeight: _isLogin() ? 0 : 60,
+                  maxHeight: _isLogin() ? 0 : 120,
                 ),
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.linear,
+                child: FadeTransition(
+                  opacity: _opacityAnimation!,
+                  child: SlideTransition(
+                    position: _slideAnimation!,
+                    child: TextFormField(
+                      decoration:
+                          const InputDecoration(labelText: 'Confirmar Senha'),
+                      keyboardType: TextInputType.emailAddress,
+                      obscureText: true,
+                      validator: _isLogin()
+                          ? null
+                          : (_password) {
+                              final password = _password ?? '';
+                              if (password != _passwordController.text) {
+                                return 'Senhas informadas não conferem.';
+                              }
+                              return null;
+                            },
+                    ),
+                  ),
+                ),
+              ),
               const SizedBox(
                 height: 20,
               ),
@@ -213,9 +232,11 @@ class _AuthFormState extends State<AuthForm>
               const Spacer(),
               TextButton(
                 onPressed: _switchAuthMode,
-                child: Text(_isLogin()
-                    ? 'Deseja fazer o cadastro?'
-                    : 'Já possuo uma conta'),
+                child: Text(
+                  _isLogin()
+                      ? 'Deseja fazer o cadastro?'
+                      : 'Já possuo uma conta',
+                ),
               )
             ],
           ),
--- projects/shop/lib/pages/auth_page.dart
+++ projects/shop/lib/pages/auth_page.dart
@@ -23,41 +23,46 @@ class AuthPage extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(bottom: 20),
-                  //cascade operator
-                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 70,
-                  ),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.purpleAccent,
-                      boxShadow: const [
-                        BoxShadow(
-                          blurRadius: 8,
-                          color: Colors.black26,
-                          offset: Offset(0, 2),
-                        )
-                      ]),
-                  child: const Text(
-                    'Minha Loja',
-                    style: TextStyle(
-                      fontSize: 45,
-                      fontFamily: 'Anton',
-                      color: Colors.white,
+          Center(
+            child: SingleChildScrollView(
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 20),
+                      //cascade operator
+                      transform: Matrix4.rotationZ(-8 * pi / 180)
+                        ..translate(-10.0),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 70,
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.purpleAccent,
+                          boxShadow: const [
+                            BoxShadow(
+                              blurRadius: 8,
+                              color: Colors.black26,
+                              offset: Offset(0, 2),
+                            )
+                          ]),
+                      child: const Text(
+                        'Minha Loja',
+                        style: TextStyle(
+                          fontSize: 45,
+                          fontFamily: 'Anton',
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
+                    AuthForm(),
+                  ],
                 ),
-                AuthForm(),
-              ],
+              ),
             ),
           ),
         ],
