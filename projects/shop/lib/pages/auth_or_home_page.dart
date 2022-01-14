953a31d0fe0fc4c3f770131dd99c20d136369026
Amanda Cleto <amanda.clet@gmail.com>
2022-01-14 11:23:59 -0300

    auto login

--- projects/shop/lib/pages/auth_or_home_page.dart
+++ projects/shop/lib/pages/auth_or_home_page.dart
@@ -10,6 +10,26 @@ class AuthOrHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Auth auth = Provider.of(context);
-    return auth.isAuth ? const ProductsOverviewPage() : const AuthPage();
+
+    return FutureBuilder(
+      future: auth.tryAutoLogin(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Theme.of(context).primaryColor,
+            ),
+          );
+        } else if (snapshot.error != null) {
+          return const Center(
+            child: Text(
+              'Ocorreu um erro!',
+            ),
+          );
+        } else {
+          return auth.isAuth ? const ProductsOverviewPage() : const AuthPage();
+        }
+      },
+    );
   }
 }
