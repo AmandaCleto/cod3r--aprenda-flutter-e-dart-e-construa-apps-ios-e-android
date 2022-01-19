df5886af9393b3703c54e58d1a2bf540c4ab1aef
Amanda Cleto <amanda.clet@gmail.com>
2022-01-19 17:51:38 -0300

    created loading page and chat page

---
+++ projects/chat/lib/pages/loading_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircularProgressIndicator(),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Carregando...',
+              style: TextStyle(
+                color: Theme.of(context).textTheme.bodyText1?.color,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
