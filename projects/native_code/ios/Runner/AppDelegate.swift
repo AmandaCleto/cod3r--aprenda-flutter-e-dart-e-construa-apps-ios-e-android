81033f275a261e788574f45ef272fc0055098012
Amanda Cleto <amanda.clet@gmail.com>
2022-03-17 13:03:01 -0300

    creating native code for ios; a function that sums two values

--- projects/native_code/ios/Runner/AppDelegate.swift
+++ projects/native_code/ios/Runner/AppDelegate.swift
@@ -7,6 +7,27 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+
+    let nativeChannel = FlutterMethodChannel(name: "amanda.com.br/native", binaryMessenger: controller.binaryMessenger)
+
+    nativeChannel.setMethodCallHandler({
+      (call: FLutterMethodCall, result: @escaping FlutterResult ) -> Void in
+
+      guard call.method == "calcSum" else {
+        result(FlutterMethodNotImplemented)
+        return
+      }
+
+      if let args = call.arguments as? [String: Any],
+         let number1 = args["number1"] as? Int,
+         let number2 = args["number2"] as? Int {
+           result(number1 + number2)
+         } else {
+           result(-1)
+         }
+    })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
