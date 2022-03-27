e8b4550cf8778f9dbc26354ce50cdebc436869a5
Amanda Cleto <amanda.clet@gmail.com>
2022-03-27 20:48:36 -0300

    updating

--- projects/chat/lib/main.dart
+++ projects/chat/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:chat/core/services/notification/chat_notification_service.dart';
 import 'package:chat/pages/auth_or_app_page.dart';
-import 'package:chat/pages/auth_page.dart';
-import 'package:chat/pages/loading_page.dart';
 import 'package:chat/theme/palette.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -37,58 +35,13 @@ class Chat extends StatelessWidget {
             ),
             visualDensity: VisualDensity.adaptivePlatformDensity,
             canvasColor: Palette.customLightColor,
-            // textSelectionTheme: const TextSelectionThemeData(
-            //   cursorColor: Colors.amber,
-            //   selectionColor: Colors.amber,
-            //   selectionHandleColor: Colors.amber,
-            // ),
-            // inputDecorationTheme: const InputDecorationTheme(
-            //   focusedBorder: UnderlineInputBorder(
-            //     borderSide: BorderSide(
-            //       color: Colors.amber,
-            //     ),
-            //   ),
-            //   enabledBorder: UnderlineInputBorder(
-            //     borderSide: BorderSide(
-            //       color: Colors.amber,
-            //     ),
-            //   ),
-            //   border: UnderlineInputBorder(
-            //     borderSide: BorderSide(
-            //       color: Colors.amber,
-            //     ),
-            //   ),
-            //   labelStyle: TextStyle(
-            //     color: Colors.amber,
-            //     fontWeight: FontWeight.w500,
-            //   ),
-            // ),
             textTheme: const TextTheme(
               headline1: TextStyle(color: Palette.customDarkBlueColor),
               bodyText1: TextStyle(color: Palette.customDarkBlueColor),
               button: TextStyle(color: Palette.customDarkBlueColor),
             ),
-            // appBarTheme: const AppBarTheme(
-            //   backgroundColor: Colors.amber,
-            //   titleTextStyle: TextStyle(
-            //     color: Colors.amber,
-            //     fontSize: 18,
-            //     fontWeight: FontWeight.w500,
-            //   ),
-            //   iconTheme: IconThemeData(
-            //     color: Colors.amber,
-            //   ),
-            //   centerTitle: true,
-            // ),
-            // elevatedButtonTheme: ElevatedButtonThemeData(
-            //   style: ElevatedButton.styleFrom(
-            //     primary: Colors.amber, // background
-            //     onPrimary: Colors.amber, // foreground
-            //     onSurface: Colors.amber,
-            //   ),
-            // ),
           ),
-          home: AuthOrAppPage(),
+          home: const AuthOrAppPage(),
         ),
       ),
     );
